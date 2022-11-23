c4c2ef3be4dc0212a1da60583aeb91112a088cc5
EmmanuelLio05 <70165675+EmmanuelLio05@users.noreply.github.com>
2022-11-22 21:24:50 -0600

    [ADD] Tratamiento de texto reconocido

--- autos.py
+++ autos.py
@@ -6,48 +6,36 @@ import random
 import time
 
 recognizer = sr.Recognizer()
-
 microphone = sr.Microphone(device_index = 0)
-
 eng = pyttsx3.init()
-
-#Configuracion de la velocidad de la pronunciacion
-eng.setProperty("rate",140)
-
-#Establecer el nivel de volumen de la voz
-eng.setProperty("volume",1.0)
-#Establecemos la voz a utilizar
-eng.runAndWait()
+eng.setProperty("rate",140)#Configuracion de la velocidad de la pronunciacion
+eng.setProperty("volume",1.0)#Establecer el nivel de volumen de la voz
+eng.runAndWait()#Establecemos la voz a utilizar
 listVoices = eng.getProperty("voices")
 eng.setProperty("voice",listVoices[0].id)
-
 eng.say("RECONOCIMIENTO DE AUTOS")
 eng.runAndWait()
-
 def recognizeMicAudio():
 	palabra = ""
 	print("Escuchando placa...")
 	with microphone as source:
 		audio = recognizer.listen(source)
 		palabra = recognizer.recognize_google(audio,language='es-MX')
-
 	return palabra
-
 #print(recognizeMicAudio())
-
 conexion = mysql.connector.connect(host="localhost",user="root",passwd="",database="autos")
 cursor = conexion.cursor(buffered=True)
-
-
 palabra = ""
 
 while palabra != "silencio":
 	palabra = recognizeMicAudio()
 	print(palabra)
+	palabra = palabra.replace("silecio", "")
+	palabra = palabra.replace(" ", "")
+	palabra = palabra.upper()
 	cursor.execute("SELECT * FROM placas WHERE id_placa like'%"+palabra+"%' ")
 	nDatos = cursor.rowcount
 	print("Se han encontrado",nDatos)
-
 	for fila in cursor:
 		ide = fila[0]
 		entrada = fila[1]
